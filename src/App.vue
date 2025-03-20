3c6919ecf6d5223c877b9b018759338b4b5ac83b
hello <feoy1234@gmail.com>
2025-03-20 22:58:17 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { onBeforeMount, ref } from 'vue';
+
+const users = ref([])
+const isLoading = ref(false)
+
+  onBeforeMount(async () => {
+    isLoading.value = true
+    const host = 'lvm66125.sit.kmutt.ac.th'
+    const res = await fetch(`http://${host}/api/users`)
+    const data = await res.json()
+    users.value = data
+    isLoading.value = false
+  })
+</script>
+
+<template>
+  <div v-if="isLoading">
+    Loading...
+  </div>
+
+  <section v-else>
+    <h1 class="text-3xl">Users</h1>d
+    <div v-for="user of users" class="border border-neutral-300 rounded-xl shadow p-3 font-bold">
+      <p>{{ user.id }}</p>
+      <p>{{ user.name }}</p>
+      <p>{{ user.email }}</p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+</style>
